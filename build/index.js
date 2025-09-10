258b5456a3c6a41c9ba9b7e0ff51f5cd563e6f9b
adamale15 <adamale@asu.edu>
2025-09-10 10:44:51 -0700

    Added CharacterCollection.ts for character sorting

--- build/index.js
+++ build/index.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
-const NumbersCollection_1 = require("./NumbersCollection");
-const collection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+const CharactersCollection_1 = require("./CharactersCollection");
+// const collection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(collection);
+// sorter.sort();
+// console.log(collection.data);
+const collection = new CharactersCollection_1.CharactersCollection("xaXtJ");
 const sorter = new Sorter_1.Sorter(collection);
 sorter.sort();
 console.log(collection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const collection = new NumbersCollection([10, 3, -5, 0]);
+// const collection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(collection);
+// sorter.sort();
+// console.log(collection.data);
+
+const collection = new CharactersCollection("xaXtJ");
 const sorter = new Sorter(collection);
 sorter.sort();
 console.log(collection.data);
