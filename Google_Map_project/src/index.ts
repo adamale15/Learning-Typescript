c095b8bf4efb5278f2a18b43f29677a99ed45666
adamale15 <adamale@asu.edu>
2025-09-12 12:57:59 -0700

    Add Google_Map_project branch contents under /Google_Map_project on main

---
+++ Google_Map_project/src/index.ts
@@ -0,0 +1,18 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+// const user = new User();
+// const company = new Company();
+
+// console.log(user);
+
+// console.log(company);
+
+import { CustomMap } from "./CustomMap";
+
+const user = new User();
+const company = new Company();
+
+const map = new CustomMap("map");
+map.addMarker(user);
+map.addMarker(company);
