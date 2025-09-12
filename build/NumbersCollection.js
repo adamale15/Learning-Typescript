0e9c725f0f7f23fc8d38991769d430b51a04d1b6
adamale15 <adamale@asu.edu>
2025-09-12 00:25:03 -0700

    Completed the sorter project by adding LinkedList sorting

--- build/NumbersCollection.js
+++ build/NumbersCollection.js
@@ -1,11 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.NumbersCollection = void 0;
-class NumbersCollection {
+const Sorter_1 = require("./Sorter");
+class NumbersCollection extends Sorter_1.Sorter {
     //   data: number[];
     constructor(data) {
-        this.data = data;
         // this.data = data;
+        super();
+        this.data = data;
     }
     get length() {
         return this.data.length;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-// const collection = new NumbersCollection([10, 3, -5, 0]);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 // const sorter = new Sorter(collection);
-// sorter.sort();
-// console.log(collection.data);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-const collection = new CharactersCollection("xaXtJ");
-const sorter = new Sorter(collection);
-sorter.sort();
-console.log(collection.data);
+const characterCollection = new CharactersCollection("xaXtJ");
+characterCollection.sort();
+console.log(characterCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(3);
+linkedList.add(-76);
+linkedList.add(543);
+linkedList.add(10);
+
+linkedList.sort();
+linkedList.print();
